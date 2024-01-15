32388526894194445fd278762549d41fb06e7cee
Yihun Alemayehu <yankure01@gmail.com>
2024-01-15 21:37:49 +0300

    fdb-02

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: TasksScreen(),
     );
   }
 }
-
--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -5,6 +5,96 @@ class TasksScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding:
+                const EdgeInsets.only(top: 60, right: 30, left: 30, bottom: 60),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30,
+                  child: Icon(
+                    Icons.list,
+                    size: 30,
+                    color: Colors.blueAccent,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Todo',
+                  style: TextStyle(
+                    fontSize: 50,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '12 tasks',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    // fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              // padding: EdgeInsets.all(0),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+                color: Colors.white,
+              ),
+              // width: MediaQuery.of(context).size.width * 1.2,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: TasksList(),
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blueAccent,
+        onPressed: () {},
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 38,
+        ),
+      ),
+    );
+  }
+
+  ListView TasksList() {
+    return ListView(
+      children: [
+        TaskTile(),
+        TaskTile(),
+        TaskTile(),
+      ],
+    );
+  }
+
+  ListTile TaskTile() {
+    return ListTile(
+      title: const Text('Buy milk'),
+      trailing: Checkbox(
+        value: false,
+        onChanged: (value) => true,
+      ),
+    );
   }
 }
